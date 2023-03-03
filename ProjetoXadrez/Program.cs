baaee0300a4d292f886edb40aa7c9954dc8f8da9
Yuri Rafael <yurilana1001@gmail.com>
2023-03-03 11:15:07 -0300

    Atualizacao na tela do tabuleiro

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -6,11 +6,17 @@ namespace ProjetoXadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(1, 0));
+                tab.colocarPeca(new Rei(Cor.Branca, tab), new Posicao(3, 0));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e) { Console.WriteLine(e.Message); }
         }
     }
 }
